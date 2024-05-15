1f7255f37ef8567bd86a1e6586e44553a4cad092
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-05-15 19:17:32 -0400

    Initial movment logic works

--- Movement.jsx
+++ Movement.jsx
@@ -6,64 +6,121 @@ var activeSequence = project.activeSequence;
 var videoTracks = activeSequence.videoTracks;
 
 var trackOne = videoTracks[0];
+var movementTypes = [
+    "Zoom in",
+    "Zoom out",
+    "Pan left",
+    "Pan right",
+    "Pan up",
+    "Pan down"
+];
+
+var movementIndex = 0;
+
 var clip = trackOne.clips[0];
 
-var components = clip.components;
-var motionComponent = components[1].properties[1];
-var positionComponent = components[1].properties[0]; 
-$.writeln(positionComponent.areKeyframesSupported());
+// var clipItem = clip.projectItem;
 
-$.writeln(motionComponent.displayName);
+// videoTracks[1].insertClip(clipItem, clip.start.ticks, 1);
 
-var xmpBlob = clip.projectItem.getProjectMetadata();
+//if tracktwo has a line asset at a certain time, do split screen is my goal
+//create a new function split and include it here whenever the two is true USE CLIP INPOINT CHECK BELOW
 
+for(var i = 0; i < trackOne.clips.length; i++) {
+  // Get the clip
+  var clip = trackOne.clips[i];
+  var components = clip.components;
+  var scaleComponent = components[1].properties[1];
+  var positionComponent = components[1].properties[0];
 
+  // Get the project metadata
+  var projectData = clip.projectItem.getProjectMetadata();
 
-// Extract the dimensions from the XMP metadata
-var dimensionsMatch = xmpBlob.match(/<premierePrivateProjectMetaData:Column\.Intrinsic\.VideoInfo>(.*?)<\/premierePrivateProjectMetaData:Column\.Intrinsic\.VideoInfo>/);
+  // Extract the dimensions from the project metadata
+  var dimensionsMatch = projectData.match(/<premierePrivateProjectMetaData:Column\.Intrinsic\.VideoInfo>(.*?)<\/premierePrivateProjectMetaData:Column\.Intrinsic\.VideoInfo>/);
+  
+  var dimensionsString = dimensionsMatch[1].match(/\d+ x \d+/);
 
-    if (dimensionsMatch) {
+    if (dimensionsString) {
         // Extract the part that contains the dimensions
-        var dimensionsString = dimensionsMatch[1].match(/\d+ x \d+/);
-        if (dimensionsString) {
-            var dimensions = dimensionsString[0].split(' x ');
-            var originalWidth = parseInt(dimensions[0]);
-            $.writeln(originalWidth);
-            var originalHeight = parseInt(dimensions[1]);
-            $.writeln(originalHeight);
+        var dimensions = dimensionsString[0].split(' x ');
+        var originalWidth = parseInt(dimensions[0]);
+        var originalHeight = parseInt(dimensions[1]);
 
-    var targetWidth = 1920;
-    var targetHeight = 1080;
+        // Set the target dimensions
+        var targetWidth = 1920;
+        var targetHeight = 1080;
 
-    var scaleFactorWidth = targetWidth / originalWidth;
+        // Calculate the scale factors
+        var scaleFactorWidth = targetWidth / originalWidth;
+        var scaleFactorHeight = targetHeight / originalHeight;
 
-    var scaleFactorHeight = targetHeight / originalHeight;
+        // Use the larger scale factor to ensure that at least one dimension is not larger than the target size
+        var scaleFactor = Math.max(scaleFactorWidth, scaleFactorHeight);
 
-    // Use the larger scale factor to ensure that at least one dimension is not larger than the target size
-    var scaleFactor = Math.max(scaleFactorWidth, scaleFactorHeight);
+        // Set the scale at the start and end of the clip
+        scaleComponent.setTimeVarying(true);
+        positionComponent.setTimeVarying(true);
 
-    motionComponent.setTimeVarying(true);
-    positionComponent.setTimeVarying(true);
+        // Set the scale at the start of the clip
+        var setScale = scaleFactor * 100; 
+        scaleComponent.setValue(setScale);
 
-    var startScale = scaleFactor * 100; // Multiply by 100 because scale is in percentage
-    var endScale = startScale + 5; // Increment the scale by 5
+        //Center the position of the clip
+        var centerPosition = [originalWidth / 2 / originalWidth, originalHeight / 2 / originalHeight]; 
+        positionComponent.setValue(centerPosition);
 
-    motionComponent.addKey(clip.inPoint.seconds);
-    motionComponent.addKey(clip.inPoint.seconds + clip.end.seconds);
+        // Set the movement type
+        var currentMovementType = movementTypes[movementIndex % movementTypes.length];
 
-    // Set the scale at each keyframe
-    motionComponent.setValueAtKey(clip.inPoint.seconds, startScale);
-    motionComponent.setValueAtKey(clip.inPoint.seconds + clip.end.seconds, endScale);
-
-    positionComponent.addKey(clip.inPoint.seconds);
-    positionComponent.addKey(clip.inPoint.seconds + clip.end.seconds);
-    
-    var centerPositionStart = [originalWidth / 2 / originalWidth, originalHeight / 2 / originalHeight]; // Center of the frame at the start
-    var centerPositionEnd = [(originalWidth / 2 + 5) / originalWidth, (originalHeight / 2 + 5) / originalHeight]; // Center of the frame at the end
-    
-    // Set the position at each keyframe to the center of the frame
-    positionComponent.setValueAtKey(clip.inPoint.seconds, centerPositionStart);
-    positionComponent.setValueAtKey(clip.inPoint.seconds + clip.end.seconds, centerPositionEnd);
-}
-
+        switch (currentMovementType) {
+            case "Zoom in":
+                scaleComponent.addKey(clip.inPoint.seconds);
+                scaleComponent.addKey(clip.inPoint.seconds + clip.end.seconds);
+                scaleComponent.setValueAtKey(clip.inPoint.seconds, setScale);
+                scaleComponent.setValueAtKey(clip.inPoint.seconds + clip.end.seconds, setScale + 5);
+                break;
+            case "Zoom out":
+                scaleComponent.addKey(clip.inPoint.seconds);
+                scaleComponent.addKey(clip.inPoint.seconds + clip.end.seconds);
+                scaleComponent.setValueAtKey(clip.inPoint.seconds, setScale + 5);
+                scaleComponent.setValueAtKey(clip.inPoint.seconds + clip.end.seconds, setScale);
+                break;
+            case "Pan left":
+                scaleComponent.addKey(clip.inPoint.seconds);
+                scaleComponent.setValueAtKey(clip.inPoint.seconds, setScale + 3);
+                positionComponent.addKey(clip.inPoint.seconds);
+                positionComponent.addKey(clip.inPoint.seconds + clip.end.seconds);
+                positionComponent.setValueAtKey(clip.inPoint.seconds, centerPosition);
+                positionComponent.setValueAtKey(clip.inPoint.seconds + clip.end.seconds, [centerPosition[0] - 0.1, centerPosition[1]]);
+                break;
+            case "Pan right":
+                scaleComponent.addKey(clip.inPoint.seconds);
+                scaleComponent.setValueAtKey(clip.inPoint.seconds, setScale + 3);
+                positionComponent.addKey(clip.inPoint.seconds);
+                positionComponent.addKey(clip.inPoint.seconds + clip.end.seconds);
+                positionComponent.setValueAtKey(clip.inPoint.seconds, centerPosition);
+                positionComponent.setValueAtKey(clip.inPoint.seconds + clip.end.seconds, [centerPosition[0] + 0.1, centerPosition[1]]);
+                break;
+            case "Pan up":
+                scaleComponent.addKey(clip.inPoint.seconds);
+                scaleComponent.setValueAtKey(clip.inPoint.seconds, setScale + 3);
+                positionComponent.addKey(clip.inPoint.seconds);
+                positionComponent.addKey(clip.inPoint.seconds + clip.end.seconds);
+                positionComponent.setValueAtKey(clip.inPoint.seconds, centerPosition);
+                positionComponent.setValueAtKey(clip.inPoint.seconds + clip.end.seconds, [centerPosition[0], centerPosition[1] - 0.1]);
+                break;
+            case "Pan down":
+                scaleComponent.addKey(clip.inPoint.seconds);
+                scaleComponent.setValueAtKey(clip.inPoint.seconds, setScale + 3);
+                positionComponent.addKey(clip.inPoint.seconds);
+                positionComponent.addKey(clip.inPoint.seconds + clip.end.seconds);
+                positionComponent.setValueAtKey(clip.inPoint.seconds, centerPosition);
+                positionComponent.setValueAtKey(clip.inPoint.seconds + clip.end.seconds, [centerPosition[0], centerPosition[1] + 0.1]);
+                break;
+        }
+            movementIndex++;
+    } else {
+        alert("Error: Could not extract dimensions from the project metadata.");
+    }
 }
