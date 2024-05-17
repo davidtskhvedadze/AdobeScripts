1d8d3ef3c01accb92f0b3a5a8fd25de79c06483f
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-05-17 12:03:41 -0400

    Update match points code

--- MatchPoints.jsx
+++ MatchPoints.jsx
@@ -5,8 +5,7 @@ var videoTracks = activeSequence.videoTracks;
 var markers = activeSequence.markers;
 
 var videoTrackOne = videoTracks[0];
-var captionTrackOld = videoTracks[3];
-var captionTrackNew = videoTracks[4];
+var captionTrack = videoTracks[5];
 
 var frameRate = 29.97; 
 var frameTime = 1 / frameRate;
@@ -23,15 +22,5 @@ for(var i = 0; i < videoTrackOne.clips.length; i++) {
         videoClip.start = startTime;
         var endTime = markers[i + 1].start.seconds + 2 * frameTime;
         videoClip.end = endTime;
-    }
-}
-
-for(var i = 0; i < captionTrackOld.clips.length; i++) {
-    var captionClipOld = captionTrackOld.clips[i];
-    var captionClipNew = captionTrackNew.clips[i];
-
-    if (captionClipOld && videoClipNew) {
-        captionClipNew.start = captionClipOld.start;
-        captionClipNew.end = captionClipNew.end;
     }
 }
